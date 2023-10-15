30042ca5e33d802030dd222c743ba51408327376
Hannah Nelson <nexec64@gmail.com>
2023-10-15 02:28:08 -0700

    cleaned up code. Styling update

--- FFT/FFT.py
+++ FFT/FFT.py
@@ -1,10 +1,9 @@
 ### 
 import numpy as np
 import logging
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 ###
-def FFT(array: np.array, forward=True, dtype='cdouble') -> None:#f
-    logging.debug("FFT called")
+def FFT(array: np.array, forward=True) -> np.array:#f
     #f docstring
     """
     performs a complex Fast Forier Transform!
@@ -16,14 +15,16 @@ def FFT(array: np.array, forward=True, dtype='cdouble') -> None:#f
         False   - converting from a frequency domain to a time domain
     """
     #d
-    #f convert to more precise datatype
-    array = np.array(array,dtype=dtype)
+    logging.debug("FFT called")
+    #f copy the array
+    array = np.array(array)
     #d
-    #f create initial modifier
+    #f create the modifier
+    #f produce the base
     initialModifier = np.e**(
             2j*np.pi
             *
-            np.arange(len(array)/2,dtype=dtype)
+            np.arange(len(array)/2)
             /
             len(array)
             )
@@ -32,10 +33,21 @@ def FFT(array: np.array, forward=True, dtype='cdouble') -> None:#f
     #f scale if forward
     if forward:
         array /= len(array)#d
+    #f conjugate if reverse
     else:
         initialModifier = np.conj(initialModifier)
+    #d
+    #d
     #f define recursive function!
-    def FFTRecursiveHelper(array: np.array,oddSegmentModifier) -> np.array:
+    def FFTRecursiveHelper(array: np.array,oddSegmentModifier) -> None:
+        #f docstring
+        """
+        Helper function which computes the whole thing
+        This function uses mutation rather than returns because it felt cleaner here
+        array - array to be mutated
+        oddSegmentModifier - the modifier, determined by the forward/backwardness of the array
+        """
+        #d
         logging.debug(f"Helper called {array=} {len(array)=}")
         #f deal with base case!
         if len(array) == 1:
@@ -52,7 +64,7 @@ def FFT(array: np.array, forward=True, dtype='cdouble') -> None:#f
         #f find results for each segment!
         #f find result for even segment!
         #f solve using recursion!
-        evenResults = FFTRecursiveHelper(evenSegments,oddSegmentModifier[0::2])
+        FFTRecursiveHelper(evenSegments,oddSegmentModifier[0::2])
         #d
         #d
         #f find result for odd segment!
@@ -60,26 +72,28 @@ def FFT(array: np.array, forward=True, dtype='cdouble') -> None:#f
         oddSegments = np.multiply(oddSegments,oddSegmentModifier)
         #d
         #f solve using recursion!
-        oddResults = FFTRecursiveHelper(oddSegments,oddSegmentModifier[0::2])
+        FFTRecursiveHelper(oddSegments,oddSegmentModifier[0::2])
         #d
         #d
         #d
         #f interleave the results!
         #f interleave evens results
-        array[0::2] = evenResults
+        array[0::2] = evenSegments
         #d
         #f interleave odd results
-        array[1::2] = oddResults
+        array[1::2] = oddSegments
         #d
         #d
-        return array
         #d
     logging.debug("FFT helper function defined")
     #d
-    return FFTRecursiveHelper(array, initialModifier)
+    #f call recursive function
+    FFTRecursiveHelper(array,initialModifier)
+    #d
+    return array
     #d
 ###
-initialArray = np.array([1,2,3,4])
+initialArray = np.array([1.1,2.5,3.1,4.9],dtype='cdouble')
 transformedArray = FFT(initialArray)
 untransformedArray = FFT(transformedArray, forward=False)
 print(f"{initialArray=}\n{transformedArray=}\n{untransformedArray=}")

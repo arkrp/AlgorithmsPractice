eec5962874abc7c8e36cf54b5a3ccab36b7b7bbc
Hannah Nelson <nexec64@gmail.com>
2024-05-24 13:51:31 -0700

    modified heap comments

--- DataStructures/heap.py
+++ DataStructures/heap.py
@@ -1,16 +1,18 @@
-#f import stuff
+#   import stuff
 import operator
-#d
-class Heap: #f
-    """ #f
+# 
+class Heap: #  
+    """ #  
+    This is a container which allows for quick access to extreme elements.
+
     This data structure allows for the fast insertion of elements and a fast removal of the least/greatest element depending on configuration.
     
     After every call, the top of the heap is the minimum/maximum of the values in the heap. (specified by heaptype)
     """
-    #d
+    # 
     __slots__ = ('_memory','_comparator')
-    #f interface methods
-    def __init__(self, *,heaptype = 'min', values = []): #f
+    #   interface methods
+    def __init__(self, *,heaptype = 'min', values = []): #  
         """
         optional parameters:
         heaptype: determine whether this heap should prioritize min or max 
@@ -27,63 +29,61 @@ class Heap: #f
             raise ValueError("You passed an incorrect type parameter to the heap; Heap type must be either \'min\' or \'max\'!")
         for value in values:
             self.push(value)
-    #d
-    #f iterator stuff
-    def __next__(self): #f
-        """ #f
+    # 
+    def __next__(self): #  
+        """ #  
         necissary for iteration
         """
-        #d
+        # 
         if self.is_empty():
             raise StopIteration
         return self.pop()
-    #d
-    def __iter__(self): #f
-        """ #f
+    # 
+    def __iter__(self): #  
+        """ #  
         necissary for iteration
         """
-        #d
+        # 
         return self
-    #d
-    #d
-    def push(self, value): #f
-        """ #f
+    # 
+    def push(self, value): #  
+        """ #  
         Adds a value to the heap
         """
-        #d
-        #f append to end
+        # 
+        #   append to end
         self._memory.append(value)
-        #d
-        #f fix ordering at end
+        # 
+        #   fix ordering at end
         lastposition = len(self._memory)-1
         self._fix_order_upwards(lastposition)
-        #d
-    #d
-    def pop(self): #f
+        # 
+    # 
+    def pop(self): #  
         """
         Removes a value from the top of the heap
         """
-        #f declare variables!
+        #   declare variables!
         memory = self._memory
         start = 0
         end = len(memory) - 1
-        #d
-        #f swap the root with the last node
+        # 
+        #   swap the root with the last node
         temp = memory[start]
         memory[start] = memory[end]
         memory[end] = temp
-        #d
-        #f remove the former root
+        # 
+        #   remove the former root
         returnvalue = memory.pop()
-        #d
-        #f fix the root borked heap
+        # 
+        #   fix the root borked heap
         self._fix_order_downwards(0)
-        #d
-        #f return the former root
+        # 
+        #   return the former root
         return returnvalue
-        #d
-    #d
-    def peek(self):
+        # 
+    # 
+    def peek(self): #  
         """
         Gets the top value of the heap
         """
@@ -91,122 +91,124 @@ class Heap: #f
             return self._memory[0]
         except IndexError as e:
             raise IndexError("Peek was called on an empty heap. Peek requires that there be at lease one element in the heap!") from e
-    def is_empty(self): #f
+    # 
+    def is_empty(self): #  
         """
         Gets whether the heap is empty
         """
-        return len(self._memory) == 0 #d
-    def copy(self): #f
+        return len(self._memory) == 0 # 
+    def copy(self): #  
         """
         Creates a shallow copy of the heap
         """
-        return Heap(values = self._memory.copy()) #d
-    #d
-    #f helper methods
-    #f private methods
-    def _fix_order_upwards(self, position): #f
-        """ #f
+        return Heap(values = self._memory.copy()) # 
+    # 
+    #   helper methods
+    #   private methods
+    def _fix_order_upwards(self, position): #  
+        """ #  
         Fixes a value being too low on the heap
         """
-        #d
+        # 
         memory = self._memory
-        #f if we are on top then there is nothing to fix!
+        #   if we are on top then there is nothing to fix!
         if position == 0:
             return
-        #d
-        #f if we are out of order, fix the order!
-        #f locate the parent node
+        # 
+        #   if we are out of order, fix the order!
+        #   locate the parent node
         parentposition = self.getParentPosition(position)
-        #d
-        #f deal with being out of order
+        # 
+        #   deal with being out of order
         if self._comparator(memory[position],memory[parentposition]):
-            #f swap upwards
+            #   swap upwards
             temp = memory[parentposition]
             memory[parentposition] = memory[position]
             memory[position] = temp
-            #d
-            #f fix the parent position
+            # 
+            #   fix the parent position
             self._fix_order_upwards(parentposition)
-            #d
-        #d
-        #d
-        #d
-    def _fix_order_downwards(self, position): #f
-        """ #f
+            # 
+        # 
+        # 
+        # 
+    def _fix_order_downwards(self, position): #  
+        """ #  
         fix a node being higher than it should be
         """
-        #d
+        # 
         memory = self._memory
-        #f find the children
+        #   find the children
         left = self.getLeftChildPosition(position)
         right = self.getRightChildPosition(position)
-        #d
-        #f deal with having no children
+        # 
+        #   deal with having no children
         if left >= len(memory):
             return
-        #d
-        #f deal with having two children
+        # 
+        #   deal with having two children
         if right < len(memory):
-            #f deal with parent lowest (or equal)
+            #   deal with parent lowest (or equal)
             if (self._comparator(memory[position],memory[right]) and
                 self._comparator(memory[position],memory[left])):
                 return
-            #d
-            #f deal with right child lowest
+            # 
+            #   deal with right child lowest
             if self._comparator(memory[right],memory[left]):
-                #f swap with right child
+                #   swap with right child
                 temp = memory[position]
                 memory[position] = memory[right]
                 memory[right] = temp
-                #d
-                #f fix right child
+                # 
+                #   fix right child
                 self._fix_order_downwards(right)
-                #d
+                # 
                 return
-            #d
-            #f deal with left child lowest (or equal to right child)
-            #f swap with left child
+            # 
+            #   deal with left child lowest (or equal to right child)
+            #   swap with left child
             temp = memory[position]
             memory[position] = memory[left]
             memory[left] = temp
-            #d
-            #f fix left child
+            # 
+            #   fix left child
             self._fix_order_downwards(left)
-            #d
+            # 
             return
-            #d
-        #d
-        #f deal with having 1 child
-        #f deal with parent lowest (or equal)
+            # 
+        # 
+        #   deal with having 1 child
+        #   deal with parent lowest (or equal)
         if self._comparator(memory[position],memory[left]):
             return
-        #d
-        #f deal with left child lowest
-        #f swap with left child
+        # 
+        #   deal with left child lowest
+        #   swap with left child
         temp = memory[position]
         memory[position] = memory[left]
         memory[left] = temp
-        #d
-        #f fix left child
+        # 
+        #   fix left child
         self._fix_order_downwards(left)
-        #d
+        # 
         return
-        #d
-        #d #d
-    #d
-    #f static methods
+        # 
+        #  # 
+    # 
+    #   static methods
     @staticmethod
-    def getParentPosition(position): #f
-        return (position - 1)//2 #d
+    def getParentPosition(position): #  
+        return (position - 1)//2 # 
     @staticmethod
-    def getLeftChildPosition(position): #f
-        return position * 2 + 1 #d
+    def getLeftChildPosition(position): #  
+        return position * 2 + 1 # 
     @staticmethod
-    def getRightChildPosition(position): #f
-        return position * 2 + 2 #d
-    #d
-    #d
-#d
+    def getRightChildPosition(position): #  
+        return position * 2 + 2 # 
+    # 
+    # 
+# 
+#  test functionality!
 if __name__ == '__main__':
     print('testing heap')
     sorted_list = [1,2,3,4,5,6,7,8]
@@ -225,3 +227,4 @@ if __name__ == '__main__':
         print('heap sort sucessful!')
     else:
         print('heap sort failed!')
+# 

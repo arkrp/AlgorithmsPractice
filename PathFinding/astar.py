b15b934bfbddc2ad2d7fc57291415377aa09fa6f
Hannah Nelson <nexec64@gmail.com>
2024-06-02 00:56:47 -0700

    refactored the A Star algorithm a bit. Brung it more structurally in line with bfs and simplified its flow

--- PathFinding/astar.py
+++ PathFinding/astar.py
@@ -21,8 +21,8 @@ class AStar(ShortestPathfinder): #  
 
     If no hueristic is provided, this algorithm is equivilant to Dijkstra's algorithm.
 
-    usage:
-    shortest_path = AStar.find_path(start_node, goal_condition, hueristic=hueristic)
+    example usage:
+        shortest_path = AStar.find_path(start_node, goal_condition, hueristic=hueristic)
 
     Warning! - if any of the starting nodes meet the goal condition this will return an empty path.
     """
@@ -30,35 +30,37 @@ class AStar(ShortestPathfinder): #  
     @staticmethod #  
     def find_path(starting_nodes: [Node], goal_condition: GoalCondition, *, hueristic: Hueristic=NullHueristic) -> Path:
         """ #  
-        Finds the shortest path from any starting node to a node which meets the goal condition using the A start algorithm.
+        A Star pathfinding algorithm
 
-        starting_nodes - all nodes which the path may start from
-        goal_condition - a \'pathfinder.GoalCondition\' which tells us when we have reached our goal
-        hueristic - a \'pathfinder.Hueristic\' which biases our distance estimates to make the algorithm run faster.
+        Finds the shortest path from any starting node to a node which meets the goal condition.
+
+        starting_nodes - The list of all nodes which the path may start from
+        goal_condition - A \'pathfinder.GoalCondition\' which tells us when we have reached our goal
+        hueristic - A \'pathfinder.Hueristic\' which biases our distance estimates to make the algorithm run faster.
+
+        If no hueristic is provided, this algorithm is equivilant to Dijkstra's algorithm.
+
+        Warning! - if any of the starting nodes meet the goal condition this will return an empty path.
         """
         # 
         #  declare data structures
-        #   stores the places we have seen an edge to and are yet to explore
+            #stores the places we have seen an edge to and are yet to explore
         unexplored = Heap()
-        #   stores all the places we have explored
+            #stores all the places we have explored
         explored = set()
-        #   stores the optimal edge leading to each node
-        incoming_edge: dict[Node, Edge] = dict()
-        #   stores the shortest distance to each node
+            #stores the optimal edge leading to each node
+        discovering_edge: dict[Node, Edge] = dict()
+            #stores the shortest distance to each node
         minimum_distance: dict[Node, float] = dict()
         # 
         #  declare state variables!
-        #   records whether we have found a path to our destination
+            #records whether we have found a path to our destination
         path_found = False
-        #   records the final node of our path to the destination
+            #records the final node of our path to the destination
         path_final_node = None
         # 
-        #  retreive problem specific functions
-        hueristic = hueristic.hueristic
-        is_goal = goal_condition.is_goal
-        # 
-        #  define exploration function
-        def explore(node: Node):
+        #  define exploration functions
+        def explore_node(node: Node): #  
             """ #  
             finds the outgoing edges of a node and adds them to the exploration list
             """
@@ -67,7 +69,7 @@ class AStar(ShortestPathfinder): #  
             explored.add(node)
             # 
             #  deal with reaching our destination!
-            if is_goal(node):
+            if goal_condition.is_goal(node):
                 nonlocal path_found
                 nonlocal path_final_node
                 path_found = True
@@ -81,44 +83,61 @@ class AStar(ShortestPathfinder): #  
                     edge,
                     minimum_distance[edge.source]+
                     edge.cost+
-                    hueristic(edge.destination)
+                    hueristic.hueristic(edge.destination)
                     ))
             # 
         # 
-        #   explore all starting nodes
-        for node in starting_nodes:
-            incoming_edge[node] = None
-            minimum_distance[node] = 0
-            explore(node)
-        # 
-        #  explore nodes until we reach our destination!
-        while not (path_found or unexplored.is_empty()):
-            #  unpack our exploration vector
-            vector = unexplored.pop()
-            edge = vector.edge
-            source_node = edge.source
-            destination_node = edge.destination
+        def explore_edge(edge: Edge):
+            """ #  
+            Explores an edge
+
+            Exploring an edge checks if the edge's destination node is explored. If the node hasn't been explored yet then we do the following three things:
+            1. register the edge as the discovering edge of the node
+            2. record the minimum distance to the node
+            3. explore the node!
+            """
             # 
-            #  deal with the node already being explored
-            if destination_node in explored:
-                continue
+            #  deal with the destination node already being explored
+            if edge.destination in explored:
+                return
             # 
             #  record path information about the destination
-            incoming_edge[destination_node] = edge
-            minimum_distance[destination_node] = edge.cost + minimum_distance[edge.source]
+            discovering_edge[edge.destination] = edge
+            minimum_distance[edge.destination] = edge.cost + minimum_distance[edge.source]
             # 
             #  explore the destination!
-            explore(destination_node)
+            explore_node(edge.destination)
             # 
+        # 
+        #  initialize values for starting nodes!
+        for node in starting_nodes:
+            discovering_edge[node] = None
+            minimum_distance[node] = 0
+        # 
+        #   explore all starting nodes
+        for node in starting_nodes:
+            explore_node(node)
+        # 
+        #  explore until we reach our destination!
+        while not (path_found or unexplored.is_empty()):
+            #  get our edge!
+            vector = unexplored.pop()
+            edge = vector.edge
+            # 
+            #  explore the edge!
+            explore_edge(edge)
+            # 
+        # 
+        #  deal with no path existing
+        if not path_found:
+            raise NoPathError('Pathfinding algorithm found no path from source to destination')
         # 
         #  construct the optimal path from the information we aquired!
         path = Path()
         current_node = path_final_node
-        if current_node == None:
-            raise NoPathError('Pathfinding algorithm found no path from source to destination')
-        while incoming_edge[current_node] != None:
-            path.append_front_edge(incoming_edge[current_node])
-            current_node = incoming_edge[current_node].source
+        while discovering_edge[current_node] != None:
+            path.append_front_edge(discovering_edge[current_node])
+            current_node = discovering_edge[current_node].source
         return path
         # 
     # 
